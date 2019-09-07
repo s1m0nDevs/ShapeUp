ad1113bbba3ad27d6590731a762600ab13201adb
Nikita <s1m0nkyivstar@gmail.com>
2019-09-07 14:00:57 +0300

    header onscroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,87 @@
+let scrollHeader = ()=>{
+  if (window.pageYOffset > 50)
+    document.querySelector('.header').classList.add('top-nav-collapse')
+  else
+    document.querySelector('.header').classList.remove('top-nav-collapse')
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  scrollHeader()
+});
+
+document.querySelectorAll('nav li').forEach(s=>{
+  s.addEventListener('click', event => {
+    event.preventDefault();
+    let target = event.target.parentElement.dataset.value;
+
+    document.querySelector(`section[data-value='${target}']`).scrollIntoView({
+       block: "center",
+       behavior: "smooth"
+     })
+  })
+})
+
+let removeClassAttr = (el)=>{
+  let classAv = el.getAttribute('class');
+  if(classAv == null || classAv == '')
+    el.removeAttribute('class');
+}
+
+
+// Nav header get info start
+var bodyRect = document.body.getBoundingClientRect(),
+  navLis = document.querySelectorAll('nav li'),
+  outPutValue = [],
+  heightSections = [],
+  numSec = 0;
+  console.log(navLis)
+
+for (var i = 0; i < navLis.length; i++)
+{
+  outPutValue[i] = navLis[i].dataset.value;
+  heightSections[i] = document.querySelector(`section[data-value='${outPutValue[i]}']`).getBoundingClientRect().top - bodyRect.top-115;
+  console.log(heightSections[i])
+}
+// Nav header get info end
+
+// Nav active by section scroll start
+document.addEventListener('scroll',(e)=>{
+  var navLiActive = document.querySelector('nav li.active');
+
+  scrollHeader()
+
+  while (numSec < Object.keys(heightSections).length)
+  {
+    if (pageYOffset < heightSections[numSec+1])
+    {
+      if (navLiActive.dataset.value != outPutValue[numSec])
+      {
+        navLiActive.classList.remove('active');
+        removeClassAttr(navLiActive);
+        navLis[numSec].classList.add('active');
+      }
+
+      numSec = heightSections.length;
+    }
+    else
+    {
+      if(pageYOffset > heightSections[Object.keys(heightSections).length-1])
+      {
+        numSec = Object.keys(heightSections).length;
+        navLiActive.classList.remove('active');
+        removeClassAttr(navLiActive);
+        navLis[numSec-1].classList.add('active');
+      }
+      numSec++;
+    }
+  }
+
+  numSec = 0;
+});
+// Nav active by section scroll end
+
+
+
 AOS.init({
   // Global settings:
   disable: 'phone', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
